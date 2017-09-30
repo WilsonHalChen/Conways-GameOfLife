854133e06cf7d666ca2e0fa6817e59e1effb2c10
WilsonHalChen <wilson.c132@gmail.com>
2017-09-30 19:45:06 -0400

    Display each step in Grow x10 and Grow x25

--- GUI.java
+++ GUI.java
@@ -12,6 +12,9 @@ import java.awt.event.ActionListener;
 import java.util.HashSet;
 import java.awt.event.ActionEvent;
 
+import java.util.concurrent.*;
+import java.lang.Runnable;
+
 public class GUI {
 
 	private JFrame frame;
@@ -44,6 +47,8 @@ public class GUI {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+		
 		frame = new JFrame();
 		int[] input = {};
 		final Calculator c = new Calculator(20, 20, input);
@@ -52,7 +57,9 @@ public class GUI {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
+
 		JButton advance1 = new JButton("Grow x1");
+		//advance 1
 		advance1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				c.advance();
@@ -63,33 +70,88 @@ public class GUI {
 		advance1.setBounds(13, 558, 171, 41);
 		frame.getContentPane().add(advance1);
 		
+		class Advance implements Runnable {
+			private int delay;
+			
+			public Advance(){
+				delay = 200;
+			}
+			public Advance(int sleepTime){
+				delay = sleepTime;
+			}
+			
+			public void run(){
+				c.advance();
+				c.validate();
+				c.repaint();
+				try{
+				Thread.sleep(delay);
+				}
+				catch(Exception e){
+					System.out.println("Advance; run(); Thread.sleep threw an Exception");
+				}
+			}
+		}
+		
 		JButton advance10 = new JButton("Grow x10");
+		//advance 10
 		advance10.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				for(int i = 0; i < 10; i++){
-					c.advance();
-					c.validate();
-					c.repaint();
-					
+					executorService.submit(new Advance());
 				}
 			}
 		});
 		advance10.setBounds(194, 558, 171, 41);
 		frame.getContentPane().add(advance10);
 		
+
 		JButton advance25 = new JButton("Grow x25");
+		//advance 25
 		advance25.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				for(int i = 0; i < 25; i++){
-					c.advance();
-					c.repaint();
+					executorService.submit(new Advance(100));
 				}
 			}
 		});
 		advance25.setBounds(372, 558, 171, 41);
 		frame.getContentPane().add(advance25);
 		
+		
+		/* EXPERIMENTAL FEATURE
+		//terminate is an array in order 
+		//to make it final so it can be added to an action listener
+		final boolean[] terminate = new boolean[1];
+		terminate[0] = false;
+		
+		JButton advanceC = new JButton("Grow Continually");
+		//advance Continually
+		advanceC.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				while(!terminate[0]){
+					executorService.submit(new Advance());
+				}
+			}
+		});
+		advanceC.setBounds(474, 312, 308, 41);
+		frame.getContentPane().add(advanceC);
+		
+		
+		JButton stopAdvance = new JButton("Stop Continual Growth");
+		//stop Growth
+		stopAdvance.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				terminate[0] = true;
+			}
+		});
+		stopAdvance.setBounds(474, 368, 308, 41);
+		frame.getContentPane().add(stopAdvance);
+		*/
+		
+		
 		JButton reset = new JButton("Clear");
+		//reset
 		reset.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				c.reset();
@@ -99,15 +161,18 @@ public class GUI {
 		reset.setBounds(555, 558, 171, 41);
 		frame.getContentPane().add(reset);
 		
+		
 		textField = new JTextField();
 		textField.setBounds(13, 512, 903, 39);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
+		
 		JLabel lblNewLabel = new JLabel("Ex: to enter initial points (0,0), (1,2), (3,6) enter: \"0 0 1 2 3 6\" (20x20 board)");
 		lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 28));
 		lblNewLabel.setBounds(13, 472, 872, 33);
 		frame.getContentPane().add(lblNewLabel);
+		
 		
 		JButton setupBoard = new JButton("Initialize");
 		setupBoard.addActionListener(new ActionListener() {
